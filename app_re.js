9bb5b7d1e5e8d102438262f8b0d0811ab31717f3
Team 1C4I <luna20617@gmail.com>
2024-12-05 06:46:03 +0000

    커밋 메시지

--- app_re.js
+++ app_re.js
@@ -4,11 +4,15 @@ var hashmap = require("hashmap");
 var config = require("config");
 var path = require("path");
 var bodyParser = require("body-parser");
+var cors = require("cors");
+var fs = require("fs");
+
 
 var app = express();
 var map = new hashmap();
 
 app.use(bodyParser.json({ type: ["application/*+json", "application/json"] }));
+app.use(cors());
 
 // Define the static file path
 app.use(express.static(__dirname + "/public"));
@@ -27,7 +31,35 @@ app.get("/seat", function (req, res) {
   res.sendFile(path.join(__dirname + "/public/seat.html"));
 });
 
+const userDB = JSON.parse(fs.readFileSync("userDB.json", "utf-8"));
+// 로그인 API
+app.post("/api/login", (req, res) => {
+  const { loginId, loginPassword } = req.body;
+
+  const user = userDB.find(
+    (user) => user.id === loginId && user.password === loginPassword
+  );
+
+  if (user) {
+    res.status(200).json({
+      success: true,
+      message: "로그인 성공",
+      user: {
+        name: user.name,
+        role: user.role,
+      },
+    });
+  } else {
+    res.status(401).json({
+      success: false,
+      message: "아이디 또는 비밀번호가 잘못되었습니다.",
+    });
+  }
+});
+
+
 app.listen(config.app.port, function () {
   console.log("Simulator API listening on port " + config.app.port);
 });
+
 
